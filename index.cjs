8167251371409a8278ce3a61f9abcc162531fcd9
Darshan <darshanas1234@gmail.com>
2025-07-18 20:36:29 +0530

    done --- 20 : 36

--- index.cjs
+++ index.cjs
@@ -14,8 +14,7 @@ app.get("/screenshot", async (req, res) => {
 
   try {
     const browser = await puppeteer.launch({
-      headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      executablePath: "/path/to/Chrome",
     });
 
     const page = await browser.newPage();
